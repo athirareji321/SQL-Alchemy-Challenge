ab3c722d6f0ceab6830e2802dd84fbb298e0a199
athirareji321 <athirareji321@gmail.com>
2023-10-03 00:14:02 +0800

    updating files

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -48,49 +48,84 @@ def home():
     )
 @app.route('/api/v1.0/precipitation')
 def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Querying the most recent date column of the Measurement table and converting todate type
     a_year_ago = dt.date(2017,8,23) - dt.timedelta(365)
-    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
+    
+    # Querrying date and precipitation columns from the measurement tables         
+    precipitation_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= a_year_ago).all()
     session.close()
+    
+    # Converting the results into a dictionary     
     precipitation_dict = {}
+    
     for date, prcp in precipitation_data:
         precipitation_dict[date] = prcp
+        
+    # Returing the results as a JSON         
     return jsonify(precipitation_dict)
 
 @app.route('/api/v1.0/stations')
 def station():
+
+    # Querrying station columns from the station tables 
     station_data = session.query(Station.station).all()
     station_list = list(np.ravel(station_data))
+    
+    # Returing the results as a JSON  
     return jsonify(station_list)
 
 @app.route('/api/v1.0/tobs')
 def tobs():
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine) 
+    
+    # Querying the most recent date column of the Measurement table and converting todate type
     a_year_ago = dt.date(2017,8,23) - dt.timedelta(365)
+    
+    # Querying the data for the ost active station for the last one year and closing the session
     temperature_data = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= one_year_ago).all()
     session.close()
+    
+    # Converting the results into a list and returing the results as a JSON
     temperature_list = list(np.ravel(temperature_data))
     return jsonify(temperature_list)
 
 @app.route('/api/v1.0/<start>')
 def start_date(start):
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)     
+    
     start_date = start
-
+    
+    # Querying for TMIN, TAVG, and TMAX for the given start date
     temperature_stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
-
+    
+    # Converting the results into a list and returing the results as a JSON
     temperature_dict = {
         'TMIN': temperature_stats[0][0],
         'TAVG': temperature_stats[0][1],
         'TMAX': temperature_stats[0][2]
     }
-
     return jsonify(temperature_dict)
 
 @app.route('/api/v1.0/<start>/<end>')
 def start_end_date(start, end):
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
     start_date = start
     end_date = end
-
+    
+    # Querying for TMIN, TAVG, and TMAX in between the given start and end date
     temperature_stats = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
-
+    
+    # Converting the results into a list and returing the results as a JSON
     temperature_dict = {
         'TMIN': temperature_stats[0][0],
         'TAVG': temperature_stats[0][1],
